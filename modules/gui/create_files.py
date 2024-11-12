52e090ba26a5a601338585aca8652430c1de6fe7
Marco H <153363103+mhoek2@users.noreply.github.com>
2024-11-12 10:56:44 +0100

    Add allowConnections checkbox to create_files page

--- modules/gui/create_files.py
+++ modules/gui/create_files.py
@@ -1,9 +1,14 @@
+from modules.app.helper import Vector2
+
 from modules.gui.gui_module import GuiModule
 
 from tkinter import *
 from tkinter.font import BOLD
 
 class GUI_CreateFiles( GuiModule ):
+    def allowConnectionCheckboxCallback( self ):
+        self.settings.allowConnection = self.allowCon.get()
+
     def onStart( self ):
         lan_info = Label( self, text=f"LAN Address: {self.settings.server_ip}:{self.settings.tcp_port}" )
         lan_info.configure(font=("Helvetica", 14, "bold"))
@@ -12,6 +17,9 @@ class GUI_CreateFiles( GuiModule ):
         header = Label( self, text="Create files.." )
         header.pack()
 
-        button = Button( self, text = "Home", 
-               command = lambda : self.gui.show_frame( self.gui.FRAME_SHARE_FILES ) )
-        button.pack()
+        self.current_position = Vector2( 0, 50 )
+
+        self.allowCon = IntVar( value=self.settings.allowConnection )
+        c1 = Checkbutton( self, text='Verbindingen Toestaan',variable=self.allowCon, onvalue=1, offvalue=0, 
+                        command=lambda : self.allowConnectionCheckboxCallback() )
+        c1.place( x = 15, y =  self.current_position.y )
