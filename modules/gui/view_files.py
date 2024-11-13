2a11c037f52ad231fd64e3978f90edcdf4f34b68
Marco H <153363103+mhoek2@users.noreply.github.com>
2024-11-13 13:07:10 +0100

    Begin adding multi-line text fields to view the contents of the documents in 'files' folder

--- modules/gui/view_files.py
+++ modules/gui/view_files.py
@@ -7,8 +7,23 @@ from tkinter.font import BOLD
 
 class GUI_ViewFiles( GuiModule ):
 
+
+    def drawFile( self, file ):
+        header = Label( self, text=f"{file['filename']}")
+        header.place( x = 0, y = self.current_position.y )
+
+        self.current_position.y += 25
+
+        text = Text( self , height=8, width=40) 
+        text.insert(END, file['content']) 
+        scroll = Scrollbar( self ) 
+        text.configure( yscrollcommand=scroll.set ) 
+        text.place( x = 0, y = self.current_position.y ) 
+
+        self.current_position.y += 135
+
     def onStart( self ):
-        header = Label( self, text=f"Aantal bestanden gevonden: {self.context.numShareableFiles}")
+        header = Label( self, text=f"Aantal bestanden gevonden: {self.context.read_write.numShareableFiles}")
         header.pack()
 
         self.current_position = Vector2( 0, 50 )
@@ -17,3 +32,10 @@ class GUI_ViewFiles( GuiModule ):
                command = lambda : self.gui.goHome() )
         button.place( x = self.settings.appplication_width - 130, 
                       y = self.settings.appplication_height - 40 )
+
+        # display files:
+        files = self.context.read_write.getShareableFiles()
+
+        for file in files:
+            file['gui'] = {}
+            self.drawFile( file )
