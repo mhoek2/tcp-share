1a4b9ccb29d1c37a3d023fa1fc55c1288d3beaa2
nidunc <nidunc@users.noreply.github.com>
2024-12-03 11:23:02 +0100

    Reorder the ReadWrite class, to make it less cluttered, change the getFiles function (to make a distinction between pdf and text files), added some filtering, and implemented the logic surrounding the passwords file

--- modules/app/read_write.py
+++ modules/app/read_write.py
@@ -1,3 +1,4 @@
+import json
 from pathlib import Path
 from typing import TypedDict
 
@@ -28,16 +29,16 @@ class ReadWrite:
             for file in path.glob("*"):
                 if file.is_file():
                     # Do not read the file if count_only is true
-                    contents = "".encode() if count_only else file.read_bytes()
+                    if count_only:
+                        contents = b""
+                    elif file.suffix == ".pdf":
+                        contents = file.read_bytes()
+                    else:
+                        contents = file.read_text().encode()
+
                     files.append({"filename": file.name, "contents": contents})
 
         return files
-
-    def removeFiles(self, path: Path) -> None:
-        """Remove all files in a certain directory."""
-        for file in path.glob("*"):
-            if file.is_file():
-                file.unlink()
 
     def hasTextFiles(self) -> bool:
         """Check if there are any text files."""
@@ -45,37 +46,46 @@ class ReadWrite:
         self.numShareableFiles = len(files)
         return bool(files)
 
+    def hasPasswordsFile(self) -> bool:
+        """Check whether the passwords file exists."""
+        file = Path(self.textDir).joinpath(self.settings.password_file)
+
+        return file.exists() and file.is_file and file.stat().st_size > 0
+
+    def hasPdfFiles(self) -> bool:
+        """Check if there are any text files."""
+        return bool(self.getFiles(self.pdfDir, True))
+
     def getTextFiles(self) -> list:
         """Get all text files."""
         files = self.getFiles(self.textDir)
 
         suffix = self.settings.file_encrypted_suffix
-        return list(filter(lambda item: suffix not in item["filename"], files))
+        return [
+            item
+            for item in files
+            if not any(
+                keyword in item["filename"]
+                for keyword in [suffix, self.settings.password_file, "_decrypted"]
+            )
+        ]
 
     def getEncryptedTextFiles(self) -> list:
         """Get all encrypted text files."""
         files = self.getFiles(self.textDir)
 
         suffix = self.settings.file_encrypted_suffix
-        return list(filter(lambda item: suffix in item["filename"], files))
+        return [item for item in files if suffix in item["filename"]]
 
-    def removeTextFiles(self) -> None:
-        """Remove all text files."""
-        if self.hasTextFiles:
-            self.removeFiles(self.textDir)
+    def getPasswordsFile(self) -> list:
+        """Get the contents of the password file."""
+        file_path = self.textDir.joinpath(self.settings.password_file)
 
-    def hasPdfFiles(self) -> bool:
-        """Check if there are any text files."""
-        return bool(self.getFiles(self.pdfDir, True))
+        return json.loads(file_path.read_text())
 
     def getPdfFiles(self) -> list:
         """Get all text files."""
         return self.getFiles(self.pdfDir)
-
-    def removePdfFiles(self) -> None:
-        """Remove all pdf files."""
-        if self.hasPdfFiles:
-            self.removeFiles(self.pdfDir)
 
     def writeFile(self, path: Path, contents: str) -> None:
         """Function to write content to files."""
@@ -85,16 +95,40 @@ class ReadWrite:
         path.write_text(contents)
         print(f"{path} saved with following contents:\n{contents}")
 
-    def writeTextFile(self, file_name: str, contents: str):
+    def writeTextFile(self, file_name: str, contents: str) -> None:
         """Write a text file."""
         file_path = self.textDir.joinpath(file_name)
         self.writeFile(file_path, contents)
 
-    def writePdfFile(self, file_name: str, contents: bytes):
-        """Write a text file."""
+    def writePasswordsFile(self, contents: list) -> None:
+        """Write to a passwords file."""
+        file_path = self.textDir.joinpath(self.settings.password_file)
+        self.writeFile(file_path, json.dumps(contents))
+
+    def writePdfFile(self, file_name: str, contents: bytes) -> None:
+        """Write a pdf file."""
         file_path = self.pdfDir.joinpath(file_name)
 
         file_path.parent.mkdir(parents=True, exist_ok=True)
 
         file_path.write_bytes(contents)
         print(f"{file_path} saved succesfully!")
+
+    def removeFiles(self, path: Path) -> None:
+        """Remove all files in a certain directory."""
+        for file in path.glob("*"):
+            if file.is_file():
+                file.unlink()
+
+    def removeTextFiles(self) -> None:
+        """
+        Remove all text files (including encrypted files, decrypted
+        files and the passwords file).
+        """
+        if self.hasTextFiles:
+            self.removeFiles(self.textDir)
+
+    def removePdfFiles(self) -> None:
+        """Remove all pdf files."""
+        if self.hasPdfFiles:
+            self.removeFiles(self.pdfDir)
