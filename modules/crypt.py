c964dc83c663590e4972953d3f90b4e0fcfb0491
nidunc <nidunc@users.noreply.github.com>
2024-12-05 14:07:10 +0100

    Numerous changes to the Crypt module: - Remove unneeded/commented functions and code (imports, for example); - Clean up existing functions (maybe a bit too much in some places—I can always expand the code again later); - Add a `make_passwords_file` function to handle generating the passwords file; - Make it so that `encrypt_files` also handles everything that has to happen before any files can actually be encrypted (calling the added `make_passwords_file` function if necessary), so that it can be called directly from other parts of the program; - Added some documentation to functions; - Cleaned up and added type hinting.

--- modules/crypt.py
+++ modules/crypt.py
@@ -3,26 +3,24 @@ from typing import TYPE_CHECKING
 
 from cryptography.fernet import Fernet
 
-# app core modules
-from modules.app.read_write import ReadWrite
-from modules.app.settings import Settings
-
 if TYPE_CHECKING:
     from main import Application
 
 
 class Crypt:
-    def __init__(self, context) -> None:
-        self.context: "Application" = context
-        self.settings: Settings = context.settings
+    def __init__(self, context: "Application") -> None:
+        self.context = context
+        self.settings = context.settings
         self.encrypted_suffix = self.settings.file_encrypted_suffix
-
-        self.files_range = range(0, self.settings.num_files)
 
         # self.test_encrypt()
         # self.test_decrypt()
 
     def test_encrypt(self) -> None:
+        """
+        Function to test generating the keys.
+        (Basically obsolete now, as encrypt_files does the same.)
+        """
         # 1. Generate the keys for encryption
         keys = self.generate_keys()
         # 2. Write the keys to a file
@@ -42,47 +40,71 @@ class Crypt:
         self.encrypt_files()
 
     def test_decrypt(self) -> None:
+        """
+        Function to test decrypting the files.
+        Also basically obsolete now, especially as it only calls decrypt_files()
+        """
         self.decrypt_files()
 
-    def generate_keys(self) -> list:
-        output_keys = []
-        for i in range(0, self.settings.num_salts):
+    def generate_keys(self) -> list[str]:
+        """
+        Generate and return a list of 50 keys (or whatever the value of
+        the num_salts setting is.)
+        """
+        output_keys: list[str] = []
+        for _ in range(0, self.settings.num_salts):
             key = Fernet.generate_key()
             output_keys.append(key.decode())
         return output_keys
 
-    def pick_keys(self, contents, amount: int) -> list:
-        output_picked_keys = []
-        for i in range(0, amount):
+    def pick_keys(self, contents: list[str], amount: int) -> list[str]:
+        """
+        Pick a given number of keys and return them.
+        (In principle, the amount of keys is equal to the amount of
+        unencrypted text files.)
+        """
+        output_picked_keys: list[str] = []
+        for _ in range(0, amount):
             choice = random.choice(contents)
             output_picked_keys.append(choice)
             contents.remove(choice)
         return output_picked_keys
 
-    # def read_txt(self):
-    #     with open("fake_Nick_file.txt") as f:
-    #         fake_file = f.read()
-    #         return fake_file
-
-    def read_password_file(self, i: int) -> str:
-        key_list = []
-        # with open("Picked_keys.txt") as f:
-        #     contents = f.readlines()
+    def get_password_from_file(self, i: int) -> str:
+        """Read the passwords file and return the i-th key."""
         contents = self.context.read_write.getPasswordsFile()
-        for line in contents:
-            key_list.append(line)
-        return key_list[i]
+        return contents[i]
 
-    # def read_encrypted_file(self):
-    #     with open("Encrypted_text.txt") as f:
-    #         encrypted_txt = f.read()
-    #         return encrypted_txt
+    def make_passwords_file(self, amount: int) -> None:
+        """
+        Make the passwords file, but only if it has not already been made
+        (and already contains the correct amount of passwords).
+        """
+        if (
+            self.context.read_write.hasPasswordsFile
+            and len(self.context.read_write.getPasswordsFile()) == amount
+        ):
+            return
+        # Generate the keys for encryption and write the keys to a file.
+        self.context.read_write.writePasswordsFile(self.generate_keys())
+
+        # Pick the given number of passwords (i) from the file,
+        #   and overwrite the file with those picked passwords.
+        self.context.read_write.writePasswordsFile(
+            self.pick_keys(self.context.read_write.getPasswordsFile(), amount)
+        )
 
     def encrypt_files(self) -> None:
+        """
+        Encrypt all (unencrypted) text files one by one.
+        It first calls the make_passwords_file function.
+        """
         files = self.context.read_write.getTextFiles()
 
+        self.make_passwords_file(len(files))
+
         for i, file in enumerate(files):
-            encrypted_contents = self.encrypt_txt(file, i)
+            encrypted_contents = self.encrypt_text(file["contents"], i)
 
             new_filename = file["filename"].replace(
                 ".txt", f"{self.encrypted_suffix}.txt"
@@ -91,28 +113,34 @@ class Crypt:
                 new_filename, encrypted_contents.decode()
             )
 
-    def encrypt_txt(self, file: ReadWrite.FilesDict, i: int) -> bytes:
-        unencrypted_txt = file["contents"].decode()
-        password = self.read_password_file(i)
-        f = Fernet(password)
-        encrypted_txt = f.encrypt(unencrypted_txt.encode())
-
-        return encrypted_txt
+    def encrypt_text(self, text: bytes, i: int) -> bytes:
+        """
+        Encrypt text with the given password (retrieved with get_password_from_file)
+        and return the encrypted text.
+        """
+        return Fernet(self.get_password_from_file(i)).encrypt(text)
 
     def decrypt_files(self) -> None:
+        """
+        Decrypt all text files one by one.
+        (For now, the decrypted files are being saved with the suffix _decrypted,
+        but this can be changed to save without any suffix—
+        overwriting the original text files, if those were present.)
+        It may be handy to first check whether the passwords file actually exists.
+        """
         files = self.context.read_write.getEncryptedTextFiles()
 
         for i, file in enumerate(files):
-            decrypted_contents = self.decrypt_txt(file, i)
+            decrypted_contents = self.decrypt_text(file["contents"], i)
 
             new_filename = file["filename"].replace(self.encrypted_suffix, "_decrypted")
             self.context.read_write.writeTextFile(
                 new_filename, decrypted_contents.decode()
             )
 
-    def decrypt_txt(self, file: ReadWrite.FilesDict, i: int) -> bytes:
-        key = self.read_password_file(i)
-        f = Fernet(key)
-        encrypted_txt = file["contents"]
-        decrypted_data = f.decrypt(encrypted_txt)
-        return decrypted_data
+    def decrypt_text(self, text: bytes, i: int) -> bytes:
+        """
+        Decrypt text with the given password (retrieved with get_password_from_file)
+        and return the decrypted text.
+        """
+        return Fernet(self.get_password_from_file(i)).decrypt(text)
