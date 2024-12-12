66a96ec2e3f2faf70523516d57e11f749d628255
Marco H <153363103+mhoek2@users.noreply.github.com>
2024-12-12 13:31:16 +0100

    Sort logs by devices and print separate tables

--- modules/gui/view_logs.py
+++ modules/gui/view_logs.py
@@ -14,30 +14,58 @@ class GUI_ViewLogs( GuiModule ):
         header_frame = Frame(frame)
         header_frame.pack(side=TOP, fill='x', pady=0, padx=(0,20))
 
-        table = Frame(header_frame)
-        table.pack(fill=BOTH, expand=1)
-
-        # Create the columns
-        ip_label = Label(table, text="IP", width=15)
-        ip_label.grid(row=0, column=0)
-        date_label = Label(table, text="Date", width=15)
-        date_label.grid(row=0, column=1)
-        content_label = Label(table, text="Content", width=30)
-        content_label.grid(row=0, column=2)
-
         log = self.context.read_write.getLogFile()
-        for i, entry in enumerate(log):
-            ip_entry = Entry(table, width=15)
-            ip_entry.grid(row=i+1, column=0)
-            ip_entry.insert(0, f"{entry['ip']}")
 
-            date_entry = Entry(table, width=15)
-            date_entry.grid(row=i+1, column=1)
-            date_entry.insert(0, entry['comment'])
+        get_device = lambda ip: next((device for device in self.settings.LAN_devices if device['ip'] == ip), None)
 
-            content_entry = Entry(table, width=30)
-            content_entry.grid(row=i+1, column=2)
-            content_entry.insert(0, entry['datetime'])
+        # sort by device
+        entries_sorted = []
+        current_ip : str = False
+        for i,entry in enumerate(log):
+            if current_ip != entry['ip']:
+                current_ip = entry['ip']
+                entries_sorted.append( { 'device': get_device( entry['ip'] ), 
+                                         'entries': [] } )
+
+            entries_sorted[len(entries_sorted)-1]['entries'].append( i )
+
+        for i, device in enumerate(entries_sorted):
+            device_header = Frame(header_frame)
+            device_header.pack( side=TOP, fill='x', padx=(10,0), pady=(20, 0) )
+
+            device_hostname = Label( device_header, text=f"{device['device']['hostname']}")
+            device_hostname.configure(font=("Helvetica", 12, "bold"))
+            device_hostname.pack( side=LEFT)
+
+            device_ip = Label( device_header, text=f"{device['device']['ip']}")
+            device_ip.configure(font=("Helvetica", 9, "normal"))
+            device_ip.pack( side=LEFT)
+
+            table = Frame(header_frame)
+            table.pack(side=TOP, fill=BOTH, expand=1)
+
+            # Create the columns
+            file_label = Label(table, text="File", width=15)
+            file_label.grid(row=0, column=0)
+            date_label = Label(table, text="Date", width=15)
+            date_label.grid(row=0, column=1)
+            content_label = Label(table, text="Content", width=30)
+            content_label.grid(row=0, column=2)
+
+            for i, entry_id in enumerate(device['entries']):
+                entry = log[entry_id]
+
+                file = Entry(table, width=15)
+                file.grid(row=i+1, column=0)
+                file.insert(0, f"{entry['file']}")
+
+                content = Entry(table, width=15)
+                content.grid(row=i+1, column=1)
+                content.insert(0, entry['comment'])
+
+                date = Entry(table, width=30)
+                date.grid(row=i+1, column=2)
+                date.insert(0, entry['datetime'])
 
         num_entries = Label( frame, text=f"Regels: {len(log)}")
         num_entries.pack()
